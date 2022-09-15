59685bfd963db86e16a394e3377042d4538724b0
Rafael Herrera <rafaelherrera731@gmail.com>
2022-09-14 22:24:05 -0700

    adding descriptions to project cards

--- rhportfolio/src/components/Pricing.jsx
+++ rhportfolio/src/components/Pricing.jsx
@@ -13,7 +13,9 @@ import {
   import { CheckIcon } from '../icons/Icons';
 
   import ThoughtsForShots from '../assets/thoughtsforshots-screenshot.png'
-  
+  import Dotify from '../assets/dotify-screenshot.png'
+  import teamScreenshot from '../assets/team-screenshot.png'
+  import zenten from '../assets/zenten.png'
   export const ListItem = (props: StackProps) => {
     const { children, ...rest } = props;
     return (
@@ -30,10 +32,10 @@ import {
         <Box
           maxW='994px'
           margin='auto'
-          mt='-40'
+          mt='-160'
           borderRadius='xl'
           overflow='hidden'
-          boxShadow='0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'
+          boxShadow='dark-lg'
           textAlign='center'
         >
           <Flex direction={['column', 'column', 'row']}>
@@ -79,7 +81,7 @@ import {
               
               <Stack direction='row'>
   
-  <Image boxSize='250px' ml="15px" src={ThoughtsForShots} alt='Dan Abramov' >
+  <Image boxSize='250px' ml="15px" src={Dotify} alt='Dan Abramov' >
     
   </Image>
 </Stack>
@@ -95,11 +97,11 @@ import {
             </Box>
             <Box p={['32px', '32px', '60px']} fontSize='lg' bg='white'>
               <Text textAlign='left'>
-                Thoughts for shots is a social media concept that allows users to create an account and post a thought. Their thoughts can be seen by their friends, those users can then buy their friends a "shot" for their "thoughts"
+                Dotify is an appplication that allows users to create a profile in order to take a music questionnaire. Application allows user to select two options and will route to a page to a song that matches the selection chosen
               </Text>
               <Stack as='ul' spacing='5' pt='6'>
-                <ListItem>Chakra UI Framework</ListItem>
-                <ListItem>MERN</ListItem>
+                <ListItem>CSS</ListItem>
+                <ListItem>SQL</ListItem>
                 <ListItem>Github</ListItem>
                 <ListItem>Slack</ListItem>
               </Stack>
@@ -108,12 +110,12 @@ import {
           <Flex direction={['column', 'column', 'row']}>
             <Box bg='#F0EAFB' p={['28px', '6opx', '60px']}>
               <Text fontSize='xl' fontWeight='extrabold'>
-                ZenTen
+                Team Generator
               </Text>
               
               <Stack direction='row'>
   
-  <Image boxSize='250px' ml="15px" src={ThoughtsForShots} alt='Dan Abramov' >
+  <Image boxSize='250px' ml="15px" src={teamScreenshot} alt='Dan Abramov' >
     
   </Image>
 </Stack>
@@ -129,11 +131,11 @@ import {
             </Box>
             <Box p={['32px', '32px', '60px']} fontSize='lg' bg='white'>
               <Text textAlign='left'>
-                Thoughts for shots is a social media concept that allows users to create an account and post a thought. Their thoughts can be seen by their friends, those users can then buy their friends a "shot" for their "thoughts"
+                This application is a team builder website. It uses the terminal to prompt questions to the user to build a team. Once all the inputs have been collected, an html page will be created and show on the webpage. 
               </Text>
               <Stack as='ul' spacing='5' pt='6'>
-                <ListItem>Chakra UI Framework</ListItem>
-                <ListItem>MERN</ListItem>
+                <ListItem>BootStrap</ListItem>
+                <ListItem>SQL</ListItem>
                 <ListItem>Github</ListItem>
                 <ListItem>Slack</ListItem>
               </Stack>
@@ -142,12 +144,12 @@ import {
           <Flex direction={['column', 'column', 'row']}>
             <Box bg='#F0EAFB' p={['28px', '6opx', '60px']}>
               <Text fontSize='xl' fontWeight='extrabold'>
-                Fourth Project
+                Zenten
               </Text>
               
               <Stack direction='row'>
   
-  <Image boxSize='250px' ml="15px" src={ThoughtsForShots} alt='Dan Abramov' >
+  <Image boxSize='250px' ml="15px" src={zenten} alt='Dan Abramov' >
     
   </Image>
 </Stack>
